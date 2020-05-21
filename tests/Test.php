843bc09e8260216916c2c18a7ea66f463ef964bf
Alexsander Küchler <ak@clickbar.dev>
2020-05-21 19:52:12 +0200

    Removed unused imports

--- tests/Test.php
+++ tests/Test.php
@@ -6,9 +6,6 @@ use Akuechler\Test\Models\House;
 use GeolyTests;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Support\Facades\DB;
-use Illuminate\Support\Facades\Log;
-use Illuminate\Support\Facades\Schema;
-use PHPUnit\Framework\TestCase;
 
 
 class Test extends \Orchestra\Testbench\TestCase
